e0b1717a15e9b9f803b1ff53f60ab5681f849f50
Adam Pawliński <adam.pawlinski@gmail.com>
2018-05-17 21:32:26 +0200

    init project based upon previous tasks, webpack prepared, app and title components created

---
+++ src/components/title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from '../styles/title.css';
+
+const Title = props => {
+  return (
+    <div className={style.title}>
+      <h2>To Do list</h2>
+      <span>There are still: {props.dataList} tasks on your list</span>
+    </div>
+  );
+};
+
+export default Title;
