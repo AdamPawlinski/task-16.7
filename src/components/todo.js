d92a09fedb1116f374a48bb35aae2bac121b692c
Adam Pawliński <adam.pawlinski@gmail.com>
2018-06-28 06:11:13 +0200

    still debugging ToDo

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from '../styles/ToDo.css';
 
-const ToDo = (data) => {
-      <li id={data.id} className={styles.ToDo}>
-        <h3>{data.id}</h3>
-        <span>{data.text}</span>
+const ToDo = (props) => {
+  return (
+      <li id={props.data.id} className={styles.ToDo}>
+        <h3>{props.data.id}</h3>
+        <span>{props.data.text}</span>
       </li>
+  )
 }
 
 export default ToDo;
